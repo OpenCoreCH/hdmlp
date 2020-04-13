fd20012dc3674e2b48e37da7eab0bf13d2da5148
Roman Böhringer <r.boehringer@opencore.ch>
2020-04-13 23:41:11 +0200

    Error handling for FileSystemPrefetcher initialization

--- src/FileSystemPrefetcher.cpp
+++ src/FileSystemPrefetcher.cpp
@@ -18,12 +18,26 @@ FileSystemPrefetcher::FileSystemPrefetcher(std::map<std::string, std::string> &b
     if (path.back() != '/') {
         path += '/';
     }
+    struct stat base_dir{};
+    if (!(stat(path.c_str(), &base_dir) == 0 && S_ISDIR(base_dir.st_mode))) { // NOLINT(hicpp-signed-bitwise)
+        throw std::runtime_error("Configured file system prefetching path doesn't exist or isn't a directory");
+    }
+
     path += std::to_string(job_id);
 
-    fd = open(path.c_str(), O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600); // NOLINT(hicpp-signed-bitwise)
-    lseek(fd, capacity - 1, SEEK_SET);
-    write(fd, "", 1);
+    if ((fd = open(path.c_str(), O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600)) < 0) { // NOLINT(hicpp-signed-bitwise)
+        throw std::runtime_error("Error opening file for prefetching");
+    }
+    if (lseek(fd, capacity - 1, SEEK_SET) < 0) {
+        throw std::runtime_error("Error seeking file for prefetching");
+    }
+    if (write(fd, "", 1) != 1) {
+        throw std::runtime_error("Error writing to file for prefetching");
+    }
     buffer = static_cast<char*>(mmap(nullptr, capacity, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)); // NOLINT(hicpp-signed-bitwise)
+    if (buffer == MAP_FAILED) {
+        throw std::runtime_error("Error while mmapping file");
+    }
 }
 
 FileSystemPrefetcher::~FileSystemPrefetcher() {
