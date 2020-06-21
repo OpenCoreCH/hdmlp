ca99f331abb2b55eea96a0a9e89530b6bfc8a5fa
Roman Böhringer <r.boehringer@opencore.ch>
2020-06-21 14:22:19 +0200

    Removed ThreadedDataLoader import

--- hdmlp/lib/torch/__init__.py
+++ hdmlp/lib/torch/__init__.py
@@ -1,5 +1,4 @@
 from .hdmlpdataloader import HDMLPDataLoader
 from .hdmlpfolder import HDMLPDatasetFolder, HDMLPImageFolder
-from .hdmlpthreadeddataloader import HDMLPThreadedDataLoader
 from .hdmlpimagenet import HDMLPImageNet
 from .hdmlpvision import HDMLPVisionDataset
